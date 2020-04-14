78228a323218c327a3ccfdfd7c82bc8e26a37058
Hot Stuff <hotstuff@Hots-MacBook.local>
2020-04-13 20:42:40 -0700

    started assignment

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -3,16 +3,18 @@ def selection_sort( arr ):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
         cur_index = i
-        smallest_index = cur_index
+        smallest_index = cur_index 
         # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
-             
+        # (hint, can do in 3 loc)
+        for j in range(i+1, len(arr)):
+            if (arr[smallest_index] > arr[j]):
+                smallest_index = j
+            
 
-
-
-        # TO-DO: swap
-
-
+            # TO-DO: swap
+        c = arr[cur_index]
+        arr[cur_index] = arr[smallest_index]
+        arr[smallest_index] = c
 
 
     return arr
